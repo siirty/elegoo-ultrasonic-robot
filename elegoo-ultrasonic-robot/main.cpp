5b808c2cf6b03fbe3c4fbea10dc8ceb16c93937e
Levin Schmidt <levin.schmidt24@gmail.com>
2021-06-27 16:41:36 +0200

    initial project files

---
+++ elegoo-ultrasonic-robot/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * elegoo-ultrasonic-robot.cpp
+ *
+ * Created: 27.06.2021 16:28:18
+ * Author : xleve
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
